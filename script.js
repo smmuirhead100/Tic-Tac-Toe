84c8de8842876a4e6714a5767dcb992224b3d726
Sean Muirhead <smmuirhead100@berkeley.edu>
2023-03-02 12:08:26 -0800

    alternate between X and O upon click event

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Signatures that connect to game display. 
 const space1 = document.querySelector(".container");
 space1.addEventListener('click', function (e) {
-    e.target.textContent = 'X';
+    currGame.playRound();
+    e.target.textContent = charTracker.char;
 });
 
 
@@ -16,12 +17,42 @@ const gameBoard = (() => {
 // Factory that stores player info
 const Player = (name, symbol) => {
     this.name = name;
+    let isUp = true;
     this.symbol = symbol;
-    return {name, symbol};
+    return {name, symbol, isUp};
 }
 
 // Module that manipulates gameBoard
+const startGame = () => {
+    const hasStarted = false;
+    return {hasStarted};
+}
 
 
-const Game = (() => {
-});
+const player1 = Player('George', 'X')
+const player2 = Player('Tyson', 'O')
+
+const currChar = () => {
+    this.char = 'H';
+    return {char};
+}
+
+charTracker = currChar(); 
+
+const Game = (player1, player2) => {
+    const playRound = () => {
+        if (player1.isUp) {
+            charTracker.char = player1.symbol;
+            player1.isUp = false;
+            player2.isUp = true;
+        } else if (player2.isUp) {
+            charTracker.char = player2.symbol;
+            player1.isUp = true;
+            player2.isUp = false;
+        }
+    }
+    return {playRound};
+};
+
+
+let currGame = Game(player1, player2);
